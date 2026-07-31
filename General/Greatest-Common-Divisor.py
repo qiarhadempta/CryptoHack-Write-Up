e9701749aa171227afddf76fb9d88f0e16e9670a
Chantiqia Rhadempta <107029115+qiarhadempta@users.noreply.github.com>
2026-07-31 23:39:26 +0700

    Update Greatest-Common-Divisor.py

--- General/Greatest-Common-Divisor.py
+++ General/Greatest-Common-Divisor.py
@@ -1,6 +1,15 @@
-import math
+def findGCD(a, b):
+    if a == 0:
+        return b
+    return findGCD(b % a, a)
 
-a = 66528
-b = 52920
+# Main function
+def main():
+    a, b = 35, 15
+    g = findGCD(a, b)
+    print(g)
 
-print(math.gcd(a, b))
+if __name__ == "__main__":
+    main()
+
+# reference: https://www.geeksforgeeks.org/dsa/euclidean-algorithms-basic-and-extended/
